822a255bdd146d0e7bcb0e427ce0ffe6626e24a4
= <gassai.hamza@gmail.com>
2025-01-18 13:08:15 +0100

    Custom open api config

---
+++ src/main/java/com/hahn/erms/common/openapi/OpenAPIConfig.java
@@ -0,0 +1,19 @@
+package com.hahn.erms.common.openapi;
+
+import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+
+@Configuration
+public class OpenAPIConfig {
+    
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("ERMS API")
+                        .description("Employee Resource Management System API Documentation")
+                        .version("1.0"));
+    }
+}
